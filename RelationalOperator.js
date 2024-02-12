91785e3d4e7c001e3ed1aa7cc1a5f0eadb994ef2
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 17:03:10 +0530

    Relational Operator

---
+++ RelationalOperator.js
@@ -0,0 +1,21 @@
+//console.log(10>100);
+
+let x = 30
+let y = 30
+
+let data = x<=y
+console.log("data : " + data)
+
+
+let a = "Pen"
+let b = "Pencil"
+
+let data1 = a < b
+console.log("data1 : " + data1);
+
+
+let u = 2
+let v = "1"
+
+let data2 = u === v //=== equal type and equal value
+console.log("data2 : " + data2);
