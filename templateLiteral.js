5a34fdcb6b6a07b9b23bf9dd6ff554dd281f97bf
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 18:24:42 +0530

    template Literal

---
+++ templateLiteral.js
@@ -0,0 +1,7 @@
+let num1 =  5;
+let num2 = 10;
+let result =  num1 + num2;
+console.log("Addition of " + num1 + " and " + num2 + " : " + result);
+console.log(`Addition of ${num1} and ${num2} is ${result}`);
+console.log(`hey 
+how are you?`);
