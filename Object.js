f2ccf50632454a717e0ff4e82b091aa98ff83c34
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 23:13:53 +0530

    Objects

---
+++ Object.js
@@ -0,0 +1,7 @@
+let input = 'Modal';
+
+let car =  { Company : "Tata" , Modal : "Curve", Year : 2024 , Color : "Green" , 'Speed & Avergae' : 150 };
+console.log(car.Company, car.Modal, car.Color, car['Speed & Avergae']); 
+//console.log(car.input);
+console.log(car[input]);
+
