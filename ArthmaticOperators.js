e4990db33f07dbce9a440eeaccabc0cea74cd93c
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 16:49:32 +0530

    Arthmatic Operator

---
+++ ArthmaticOperators.js
@@ -0,0 +1,22 @@
+let num1 = true
+let num2 = false
+
+let result = num1  + num2; 
+console.log("Result : " + result);
+
+
+let num = 10
+num += 5 // "Short-Hand Operator" // num = num + 5 
+//num =+ 5 //
+
+let x = num++ // Post - Increment Operator //First Assign and then increment
+
+//let x = ++num // Pre - Increment Operator // First Increment and then Assign
+
+console.log("num : " + num);
+
+
+let nump  = 4
+let r = 4 ** 4
+//let r = (Math.pow(4,3));
+console.log("r : " + r);
