e256eb88a8385cee3f6d9ae09f64f5e63481976d
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 23:42:56 +0530

    for in loop

---
+++ forinLoop.js
@@ -0,0 +1,20 @@
+let Human = {
+    Male : {
+        age : 25,
+        name : "John",
+        profession: "Software Developer",
+    },
+    Female : {
+        age : 45,
+        name : "Rosy",
+        profession: "Baker",
+    }
+};
+
+
+
+
+for (let key in Human){
+ console.log(Human[key]);
+}
+
