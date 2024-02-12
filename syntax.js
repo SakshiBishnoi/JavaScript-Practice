334df8fda863273ac968c69e56a56331f5c8ed9b
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 15:43:24 +0530

    data types

--- syntax.js
+++ syntax.js
@@ -1,1 +1,54 @@
-console.log(2+2);
+//console.log(2+2);
+let num= 2+2
+console.log(num+4);
+
+num = 104
+console.log(num);
+
+let name = "star's"
+console.log(name)
+
+
+let r = 5;
+const pi =3.15
+
+r = 10
+//pi = 2
+
+let area =  pi*(r*r)
+
+console.log("Area is : " + area)
+
+//let num1 = 1.5e12 
+//let num1 = 0xfff
+//let num1 = 5/0
+//let num1 = -5/0
+//let num1 = 100_000_00
+let num1 = 1.5e12 
+
+
+console.log("num is : " + num1);
+console.log("It is of type : " + typeof num1);
+
+console.log(Number.MAX_VALUE * 100);
+
+let num2 = 1324889758689743867790359878968914376878597985n
+console.log(num2 + 4n);
+
+
+let firstname = "Sakshi"
+let lastname = "Bishnoi"
+let user = firstname + " " + lastname
+user = " Sakshi \vBishnoi \nsays \"Hi\" \bMuchkin "
+console.log(user);
+
+
+let bool = 10>5
+console.log(bool);
+
+
+let  nullVar = null
+console.log(nullVar);
+
+let  undefVar;
+console.log(undefVar);
