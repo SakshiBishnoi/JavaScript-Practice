2a9c9b9c95fa094654da3d544fffa575db412851
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 19:22:17 +0530

    little comments

--- WhileLoop.js
+++ WhileLoop.js
@@ -1,12 +1,12 @@
 let n = 5;
-while ( n >= 0) {  
-    console.log("hello " + n );
+while ( n >= 0) {  // First check for condition 
+    console.log("hello " + n ); // then excute block
     n--;  
 }
 
 
 let num = 10;
-do{
+do{ //First excute block
     console.log("Hello" + num);
     num++;
-}while(num <= 20);
+}while(num <= 20); // Then  Check the Condition
