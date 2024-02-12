4ad29e1bb9dc0bee7518b3f0579b4d51c585d2ab
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-12 23:56:50 +0530

    Returning and Passing Function

---
+++ Func1.js
@@ -0,0 +1,9 @@
+//Reuse; for a bigger task, always create small tasks/chunks; DRY -DO NOT REPEAT YOURSELF
+
+function great(user){
+    return (`Hello, ${} How are you?`)
+}
+
+let user = "bishnoi"
+let name = "Sakshi";
+console.log(great(user)); 
