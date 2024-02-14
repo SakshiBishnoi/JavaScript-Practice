f49559097c592d9834a2682a1ff041bd424b8fd7
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-14 22:51:59 +0530

    Recursion

---
+++ Recursion.js
@@ -0,0 +1,12 @@
+// in stack Last In First Out (LIFO)
+
+//Function is calling itself that is called  Recursion 
+
+let num = 1;
+function show(){
+    console.log("hello",num);
+    //num++;
+    //if(num === 100)
+    //  show();
+}
+show();
