e36dd73010ea04d57e8e48ea8392532971526645
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-15 01:26:55 +0530

    random

--- palertPrompt.js
+++ palertPrompt.js
@@ -1,2 +1,5 @@
+//method to display a modal dialog box asking the user for input. 
+//This is often used to gather information from the user within a web browser before proceeding with an action
+
 let p = prompt("message")
 console.log(p);
