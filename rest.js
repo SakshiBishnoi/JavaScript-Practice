7677b5937d86042b83ddb73df4d0fcbdbc71f81b
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-21 17:48:10 +0530

    rest

--- rest.js
+++ rest.js
@@ -1,22 +1,24 @@
-// rest params collects all remaining arguments into an actual array
+//rest params collects all remaining arguments into an actual array
 
 function sum(){
     console.log(arguments);
+    // const numbers = Array.from(arguments); // Convert arguments to an array
+    // const total = numbers.reduce((a, b) => a + b); // Use reduce to calculate sum
+    // console.log(total);
 }
 sum(1,2,3,4,5,6);
-// reduce can not be used here 
+
 
 function total(...num){
     console.log(num.reduce((a,b)=> a+b));
 }
 total(1,2,3,4,5,6);
-//reduce is applicable to this case as it takes two parameters at a time and we are providing the whole set of args in one go
-// reduce is applicable here as it reduces the array to a single value
+//
 
-function comp(gold,silver,bronze, ...participants){
-    console.log(`Gold : ${gold}`);
-    console.log(`Silver: ${silver}`);
-    console.log(`Bronze: ${bronze}`);
+function comp(gold,silver,bronze,score, ...participants){
+    console.log(`Gold : ${gold} with score`);
+    console.log(`Silver: ${silver} with score`);
+    console.log(`Bronze: ${bronze} with score`);
     console.log('Thanks to everyone else : ', participants)
 }
-comp(10,20,30,'Rohit','Akash','Anuj');
+comp(10,20,30,'Rohit1','Akash2','Anuj');
