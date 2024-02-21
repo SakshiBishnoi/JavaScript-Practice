794af56cc607acccce4c7314efb572526c3c3cd9
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-02-21 17:56:27 +0530

    Destructuring from ArrayDestructuring

---
+++ ArrayDestructuring.js
@@ -0,0 +1,6 @@
+let score = [14245,14542,13987,16094,42654,43645,14987];
+let highScore = score[0];
+
+const [gold,silevr,bronze, ...everyone] = score; //destructuring assignment 
+console.log(gold,silevr,bronze);      
+console.log();
